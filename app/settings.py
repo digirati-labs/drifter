324b98b17cfb7745e7cfec9778e096961630aff0
Adam Christie <fractos@gmail.com>
2019-09-13 11:20:22 +0100

    default to DEBUG being True.

--- app/settings.py
+++ app/settings.py
@@ -5,7 +5,7 @@ import distutils.util
 from database.sqlite_database import SqliteDatabase
 from database.postgresql_database import PostgreSqlDatabase
 
-DEBUG = bool(distutils.util.strtobool(os.getenv("DEBUG", "False")))
+DEBUG = bool(distutils.util.strtobool(os.getenv("DEBUG", "True")))
 DB_TYPE = os.getenv("DB_TYPE")
 TERRAFORM_S3_BUCKET = os.getenv("TERRAFORM_S3_BUCKET")
 TERRAFORM_S3_KEY = os.getenv("TERRAFORM_S3_KEY")
